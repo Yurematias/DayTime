5a45329375d79c12fc6fbd3feab4bda2b880f98f
Yure Matias <yurematias@ufrn.edu.br>
2019-07-12 02:28:15 -0300

    Delete Factory-Exports.js

--- cursoCod3r/módulo-node/aula-127-Instância-Unica-vs-Nova/Factory-Exports.js
+++
@@ -1,9 +0,0 @@
-module.exports = () => {
-    return {
-        nome: "Nameless King",
-        game: "Dark Souls 3",
-        talk(){
-            console.log("I'm the first born of Gwyn");
-        }
-    }
-}
