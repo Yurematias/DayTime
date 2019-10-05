4dfc6d2b07850e8393086dcc2e92472a10ca9249
Yure Matias <ymatias2000@gmail.com>
2019-10-05 16:26:19 -0300

    Add files via upload

---
+++ cursoCod3r/modulo-ajax/server.js
@@ -0,0 +1,35 @@
+const bodyParser = require('body-parser');
+const express = require('express');
+const app = express();
+const port = 8000;
+
+app.use(express.static('.'));
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+const multer = require('multer');
+
+app.get('/teste', (req, res) => res.send('ta ok'))
+
+const storage = multer.diskStorage({
+    destination: function (req, file, callback) {
+        callback(null, './upload');
+    },
+    filename: function (req, file, callback) {
+        callback(null, `${Date.now()}_${file.originalname}`);
+    }
+});
+
+const upload = multer({ storage }).single('arquivo');
+
+app.post('/upload', (req, res) => {
+    upload(req, res, err => {
+        if (err) {
+            return res.end('Ocorreu um erro.')
+        }
+        res.end('Concluído com sucesso.')
+    })
+})
+
+
+app.listen(port, () => console.log(`Listening at port ${port}`))
